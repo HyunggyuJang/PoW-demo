2f72058bcc8ed4d26ca2186c0cf832882c022b76
Hyunggyu Jang <murasakipurplez5@gmail.com>
2023-04-21 14:38:25 +0900

    Add PoW demo file

---
+++ pow.ts
@@ -0,0 +1,31 @@
+import {createHash} from 'crypto'
+const base = '<YourHash>'
+function hash(string: string) {
+  return createHash('sha256').update(string).digest('hex');
+}
+function score(nonce: number): number {
+  let currentScore = 0
+  const hashed = hash(base + nonce)
+  for (let i = 0; i < hashed.length; i++) {
+    const bin = parseInt(hashed[i], 16).toString(2)
+    if (bin === '0') {
+      currentScore += 4
+    } else {
+      currentScore += 4 - bin.length
+      return currentScore
+    }
+  }
+  return currentScore
+}
+
+let highestNonce = 0
+let highestScore = 0
+for (let i = 0; i < 10_000_000_000; i++) { // The probability that a score >= 30 in this range is more than 99%!
+  const currentScore = score(i)
+  if (currentScore > highestScore) {
+    highestNonce = i, highestScore = currentScore
+    console.log(`the highest nonce is updated to ${highestNonce} with score of ${highestScore}`)
+  }
+}
+
+console.log(`the highest nonce is ${highestNonce} with score of ${highestScore}`)
